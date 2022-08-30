22adbc87cff7646c6163e8a2971db28979c80a66
Pedro Nicolas Costa <pedroniickolas@gmail.com>
2022-08-29 21:11:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
+    <title>Vocabulário</title>
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+            <h1>Glossário de Vocabulários</h1>
+            <h2>Construa seu vocabulário e seja feliz :)</h2>
+            <div class="grid-container">
+                <div class="grid-item">Palavras Comuns</div>
+                <div class="grid-item">Siglas</div>
+                <div class="grid-item">Nomes Próprios</div>  
+                <div class="grid-item">Palavrões</div>
+                <div class="grid-item">Gírias</div>
+                <div class="grid-item">Ditados Populares</div>  
+                <div class="grid-item">Generalizações</div>
+                <div class="grid-item">Opinião Política</div>
+                <div class="grid-item">Preconceito</div>  
+                <div class="grid-item">Clichês</div> 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
